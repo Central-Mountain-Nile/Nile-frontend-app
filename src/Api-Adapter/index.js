dd4818db0413d5310280939b6b13e4526ffd3f5f
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-03 14:03:43 -0500

    users apidone

--- src/Api-Adapter/index.js
+++ src/Api-Adapter/index.js
@@ -2,6 +2,58 @@ const BASE_URL = "https://nile-marketplace.onrender.com/api";
 
 // user endpoints
 
+export const loginUser = async (username, password) => {
+  try {
+    console.log(`${BASE_URL}/users/login`);
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const fetchMe = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
 // products endpoints
 
 // userPayments endpoints
