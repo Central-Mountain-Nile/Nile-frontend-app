6293924b821d4f5c51b2ada494e7bf51067b5d77
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-04-03 11:39:38 -0500

    settign up the front end

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Main } from "./Main";
 export { default as Navbar } from "./Navbar";
+export { default as Cart } from "./Cart";
+export { default as CartItem } from "./CartItem";
+export { default as Categories } from "./Categories";
+export { default as CheckoutPage } from "./CheckoutPage";
