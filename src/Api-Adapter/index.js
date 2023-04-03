f9d1a8019ab4ceae7d7427bd6e0456872251bce2
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-03 13:28:42 -0500

    register

---
+++ src/Api-Adapter/index.js
@@ -0,0 +1,13 @@
+const BASE_URL = "";
+
+// user endpoints
+
+// products endpoints
+
+// userPayments endpoints
+
+// cart endpoints
+
+// discounts endpoints
+
+// order endpoints
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,84 @@
 import React from "react";
-import { Navbar } from "./";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+    Navbar,
+    Login,
+    Register,
+    Home,
+    Cart,
+    CartItem,
+    Categories,
+    CheckoutPage,
+    CreateItem,
+    CreatePayment,
+    DisplayItem,
+    EditProfile,
+    Footer,
+    ItemsFeed,
+    Profile,
+    SingleItem,
+    Store,
+    UpdateItem,
+
+  } from "./";
 
 const Main = () => {
     return(
         <div id="main">
+            <BrowserRouter>
             <Navbar />
+            <Categories />
+            <Routes>
+                <Route>
+                    <Home />
+                </Route>
+                <Route>
+                    <Cart />
+                </Route>
+                <Route>
+                    <CartItem />
+                </Route>
+                <Route>
+                    <CheckoutPage />
+                </Route>
+                <Route>
+                    <CreateItem />
+                </Route>
+                <Route>
+                    <CreatePayment />
+                </Route>
+                <Route>
+                    <DisplayItem />
+                </Route>
+                <Route>
+                    <EditProfile />
+                </Route>
+                <Route>
+                    <Footer />
+                </Route>
+                <Route>
+                    <ItemsFeed />
+                </Route>
+                <Route>
+                    <Login />
+                </Route>
+                <Route>
+                    <Profile />
+                </Route>
+                <Route>
+                    <Register />
+                </Route>
+                <Route>
+                    <SingleItem />
+                </Route>
+                <Route>
+                    <Store />
+                </Route>
+                <Route>
+                    <UpdateItem />
+                </Route>
+            </Routes>
+            </BrowserRouter>
         </div>
     )
 }
