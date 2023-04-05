6e72441126d153429e18021f9ff9c074bcbcec86
MonkComputer <bmonk1996@yahoo.com>
2023-04-05 10:08:55 -0500

    aligning images

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { getCart } from '../Api-Adapter'
+import React, { useEffect, useState } from "react";
+import { getCart } from "../Api-Adapter";
 
 function Cart(props) {
-  const {token} = props
+  const { token } = props;
 
-  getCart
-//const [cart, setCart] = useState({})
+  getCart;
+  //const [cart, setCart] = useState({})
 
-// useEffect(() => {
-//   getCartItems()
-// }, []);
-const cart = {cartItems:[{id: 1,name:"product1", price:12,quantity:43},{id: 2,name:"product7", price:17,quantity:4},{id: 3,name:"product9", price:92,quantity:83}]}
-// function getCartItems(){
-//   setCart(getCart(token))
-// }
-
-
-
+  // useEffect(() => {
+  //   getCartItems()
+  // }, []);
+  const cart = {
+    cartItems: [
+      { id: 1, name: "product1", price: 12, quantity: 43 },
+      { id: 2, name: "product7", price: 17, quantity: 4 },
+      { id: 3, name: "product9", price: 92, quantity: 83 },
+    ],
+  };
+  // function getCartItems(){
+  //   setCart(getCart(token))
+  // }
 
   return (
-    <div className="cart"> <h1>Cart</h1>
-    { cart.cartItems? (
-      cart.cartItems.map((product) => {
-        return (
-          <div className="product-card-cart" key={'cart' + product.id}>
-            <h2 className="productName">{product.name}</h2>
-            <p className="productPrice">${product.price}</p>
-            <p className="cart-quantity">Quantity: {product.quantity}</p>
-            <img className="cart_img" src="http://placeimg.com/640/480/nature" alt={product.description}/>
-          </div>
-        );
-      })
-    ) : (
-      <div className="loader"></div>
-    )}
-
-      
+    <div className="cart">
+      {" "}
+      <h1>Cart</h1>
+      {cart.cartItems ? (
+        cart.cartItems.map((product) => {
+          return (
+            <div className="product-card-cart" key={"cart" + product.id}>
+              <div className="product_text"> 
+                <h2 className="productName">{product.name}</h2>
+                <p className="productPrice">${product.price}</p>
+                <p className="cart-quantity">Quantity: {product.quantity}</p>
+              </div>
+              <img
+                className="cart_img"
+                src="http://placeimg.com/640/480/nature"
+                alt={product.description}
+              />
+              <div className="product_buttons">
+                <button>remove from cart</button>
+                <button>change quantity</button>
+                </div>
+            </div>
+          );
+        })
+      ) : (
+        <div className="loader"></div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
