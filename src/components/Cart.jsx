8787d92c406b3b8bf610c413cf0c7e74c3faf3ba
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-04-07 14:13:41 -0500

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,44 +2,43 @@ import React, { useEffect, useState } from "react";
 import { deleteCartItem, getCart, loginUser } from "../Api-Adapter";
 import { useNavigate, Outlet, Link } from "react-router-dom";
 
-
 function Cart(props) {
   const { token } = props;
   getCart;
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
 
   useEffect(() => {
-    getCartItems()
+    getCartItems();
   }, []);
 
-  async function getCartItems(){
-
-    setCart(await getCart(token))
-  };
+  async function getCartItems() {
+    setCart(await getCart(token));
+  }
 
   async function removeFromCart(idx) {
     const newCart = { ...cart };
-   const result = await deleteCartItem(cart.cartItems[idx].id, token);
-   if (!result.message) {
-      newCart.cartItems = [...cart.cartItems.splice(0,idx),...cart.cartItems.splice(idx + 1)]
+    const result = await deleteCartItem(cart.cartItems[idx].id, token);
+    if (!result.message) {
+      newCart.cartItems = [
+        ...cart.cartItems.splice(0, idx),
+        ...cart.cartItems.splice(idx + 1),
+      ];
 
       setCart(newCart);
     } else {
       //display error message
     }
-
-  };
+  }
 
   function orderTotal() {
     let subtotal = 0;
     if (cart.cartItems) {
-      cart.cartItems.forEach(item => {
+      cart.cartItems.forEach((item) => {
         subtotal += item.price * item.quantity;
       });
     }
     return subtotal;
-  };
-
+  }
 
   return (
     <div className="cart">
@@ -48,37 +47,37 @@ function Cart(props) {
       {cart.cartItems ? (
         cart.cartItems.map((product, idx) => {
           return (
-          <div>
-            <div className="product-card-cart" key={"cart" + product.id}>
-              <div className="product_text">
-                <h2 className="productName">{product.name}</h2>
-                <p className="productPrice">${product.price}</p>
-                <p className="cart-quantity">Quantity: {product.quantity}</p>
+            <div>
+              <div className="product-card-cart" key={"cart" + product.id}>
+                <div className="product_text">
+                  <h2 className="productName">{product.name}</h2>
+                  <p className="productPrice">${product.price}</p>
+                  <p className="cart-quantity">Quantity: {product.quantity}</p>
+                </div>
+                <img
+                  className="cart_img"
+                  src="http://placeimg.com/640/480/nature"
+                  alt={product.description}
+                />
+                <div className="product_buttons">
+                  <button onClick={() => removeFromCart(idx)}>
+                    remove from cart
+                  </button>
+                  <input placeholder="Edit quantity" />
+                </div>
               </div>
-              <img
-                className="cart_img"
-                src="http://placeimg.com/640/480/nature"
-                alt={product.description}
-              />
-              <div className="product_buttons">
-                <button onClick={() => removeFromCart(idx)}>
-                  remove from cart
-                </button>
-                <input placeholder="Edit quantity" />
+              <div className="CartTotal">
+                <p>Subtotal: ${orderTotal()}</p>
+                <Link to="/checkoutform">
+                  <button id="Checkoutbutton">Checkout</button>
+                </Link>
               </div>
             </div>
-          </div>
           );
         })
       ) : (
         <div className="loader"></div>
       )}
-            <div className="CartTotal">
-              <p>Subtotal: ${orderTotal()}</p>
-              <Link to="/checkoutform">
-                <button id="Checkoutbutton">Checkout</button>
-                </Link>
-            </div>
     </div>
   );
 }
