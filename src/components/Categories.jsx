ac87e11f256f892f5a84a41282b2609c0d6ddbb0
MonkComputer <bmonk1996@yahoo.com>
2023-04-09 12:19:37 -0500

    added all category links

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,14 +9,14 @@ const {setCategory} = props
     <div className="categories">
       <Link to={"/itemsfeed/clothing/1"}><button>Clothing</button></Link>
       <Link to={"/itemsfeed/electronics/1"}><button>Electronics</button></Link>
-      <button>Jewelry</button>
-      <button>Music</button>
-      <button>Auto</button>
-      <button>Gaming</button>
-      <button>Pets</button>
-      <button>Sports/Lifestyle</button>
-      <button>Tools/appliances</button>
-      <button>Books</button>
+      <Link to={"/itemsfeed/Jewelry/1"}><button>Jewelry</button></Link>
+      <Link to={"/itemsfeed/music/1"}><button>Music</button></Link>
+      <Link to={"/itemsfeed/auto/1"}><button>Auto</button></Link>
+      <Link to={"/itemsfeed/gaming/1"}><button>Gaming</button></Link>
+      <Link to={"/itemsfeed/pets/1"}><button>Pets</button></Link>
+      <Link to={"/itemsfeed/sports/lifestyle/1"}><button>Sports/Lifestyle</button></Link>
+      <Link to={"/itemsfeed/tooles/appliances/1"}><button>Tools/appliances</button></Link>
+      <Link to={"/itemsfeed/books/1"}><button>Books</button></Link>
     </div>
   );
 }
