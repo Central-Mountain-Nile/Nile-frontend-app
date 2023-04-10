8f0b323b6a88e85128686bca9c8ee4c872341049
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-04-10 10:00:24 -0500

    working on css

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      {" "}
+      <img className="img" src={require("./images/banner.jpg")} alt="" />
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/index.js
+++ src/components/index.js
@@ -17,6 +17,5 @@ export { default as Register } from "./Register";
 export { default as SingleItem } from "./SingleItem";
 export { default as Store } from "./Store";
 export { default as UpdateItem } from "./UpdateItem";
-
-export { default as Admin} from "./Admin";
-
+export { default as Banner } from "./Banner";
+export { default as Admin } from "./Admin";
