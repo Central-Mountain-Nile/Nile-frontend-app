88741aec8b91a0a8c0c7b46c540ca6f8fd80ceba
MonkComputer <bmonk1996@yahoo.com>
2023-04-10 12:19:58 -0500

    change url

--- src/Api-Adapter/index.js
+++ src/Api-Adapter/index.js
@@ -1,6 +1,6 @@
-// const BASE_URL = "https://nileserver.onrender.com/api";
+const BASE_URL = "https://nileserver.onrender.com/api";
 
-const BASE_URL ="http://localhost:8080/api"
+// const BASE_URL ="http://localhost:8080/api"
 
 
 export const loginUser = async (username, password) => {
