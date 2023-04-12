27f1f49b4412db0298896152508c3651ca954f5c
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-12 09:24:55 -0500

    FINISHED-again git commit -m FINISHED

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -14,14 +14,25 @@ function Register(props) {
   const [state, setState] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [country, setCountry] = useState("");
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const setToken = props.setToken;
   const setCurrentUser = props.setCurrentUser;
 
   const navigate = useNavigate();
 
   const registerNewUser = async (username, password) => {
-    if (!username || !password ||!firstName || !lastName || !email || !address||!city||!state||!postalCode||!country) {
+    if (
+      !username ||
+      !password ||
+      !firstName ||
+      !lastName ||
+      !email ||
+      !address ||
+      !city ||
+      !state ||
+      !postalCode ||
+      !country
+    ) {
       setMessage("All fields are required");
       return;
     }
@@ -41,7 +52,7 @@ function Register(props) {
         country,
       });
       if (result.error) {
-        setMessage(result.message)
+        setMessage(result.message);
       } else {
         setToken(result.token);
         setCurrentUser(result.user);
@@ -65,106 +76,106 @@ function Register(props) {
           }}
         >
           <div className="rowWrapper">
-          <div className="row1">
-          <p>First Name:</p>
-          <input
-            className="firstNameInput"
-            value={firstName}
-            type="text"
-            placeholder="First Name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <p>Username:</p>
-          <input
-            className="userNameInput"
-            value={username}
-            type="text"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+            <div className="row1">
+              <p>First Name:</p>
+              <input
+                className="firstNameInput"
+                value={firstName}
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <p>Username:</p>
+              <input
+                className="userNameInput"
+                value={username}
+                type="text"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
 
-          <p>Password:</p>
-          <input
-            className="passwordInput"
-            value={password}
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br></br>
-          <p>Address:</p>
-          <input
-            className="addressInput"
-            value={address}
-            type="text"
-            placeholder="Address"
-            onChange={(e) => setAddress(e.target.value)}
-          />
-                    <p>City:</p>
-          <input
-            className="CityInput"
-            value={city}
-            type="text"
-            placeholder="City"
-            onChange={(e) => setCity(e.target.value)}
-          />
-                    <p>Zip:</p>
-          <input
-            className="ZipInput"
-            value={postalCode}
-            type="number"
-            placeholder="Zip"
-            onChange={(e) => setPostalCode(e.target.value)}
-          />
-          </div>
-          <div className="row2">
-                    <p>Last Name:</p>
-          <input
-            className="LastNameInput"
-            value={lastName}
-            type="text"
-            placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
-          />
-                    <p>Email:</p>
-          <input
-            className="emailInput"
-            value={email}
-            type="text"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-                    <p>Confirm Password:</p>
-          <input
-            className="passwordInput"
-            type="password"
-            placeholder="Confirm Password"
-          />
-          <p>Address line 2:</p>
-          <input
-            className="address2Input"
-            value={address2}
-            type="text"
-            placeholder="Address line 2 (optional)"
-            onChange={(e) => setAddress2(e.target.value)}
-          />
-          <p>State:</p>
-          <input
-            className="StateInput"
-            value={state}
-            type="text"
-            placeholder="State"
-            onChange={(e) => setState(e.target.value)}
-          />
-          <p>Country:</p>
-          <input
-            className="CountryInput"
-            value={country}
-            type="text"
-            placeholder="Country"
-            onChange={(e) => setCountry(e.target.value)}
-          />
-          </div>
+              <p>Password:</p>
+              <input
+                className="passwordInput"
+                value={password}
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br></br>
+              <p>Address:</p>
+              <input
+                className="addressInput"
+                value={address}
+                type="text"
+                placeholder="Address"
+                onChange={(e) => setAddress(e.target.value)}
+              />
+              <p>City:</p>
+              <input
+                className="CityInput"
+                value={city}
+                type="text"
+                placeholder="City"
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <p>Zip:</p>
+              <input
+                className="ZipInput"
+                value={postalCode}
+                type="number"
+                placeholder="Zip"
+                onChange={(e) => setPostalCode(e.target.value)}
+              />
+            </div>
+            <div className="row2">
+              <p>Last Name:</p>
+              <input
+                className="LastNameInput"
+                value={lastName}
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <p>Email:</p>
+              <input
+                className="emailInput"
+                value={email}
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <p>Confirm Password:</p>
+              <input
+                className="passwordInput"
+                type="password"
+                placeholder="Confirm Password"
+              />
+              <p>Address line 2:</p>
+              <input
+                className="address2Input"
+                value={address2}
+                type="text"
+                placeholder="Address line 2 (optional)"
+                onChange={(e) => setAddress2(e.target.value)}
+              />
+              <p>State:</p>
+              <input
+                className="StateInput"
+                value={state}
+                type="text"
+                placeholder="State"
+                onChange={(e) => setState(e.target.value)}
+              />
+              <p>Country:</p>
+              <input
+                className="CountryInput"
+                value={country}
+                type="text"
+                placeholder="Country"
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
           </div>
 
           <br></br>
@@ -178,8 +189,8 @@ function Register(props) {
         </form>
         <img className="logoFoot" src="/nileLogo.png" alt="" />
 
+        <h2 className="message">{message}</h2>
       </div>
-      <h2 className="message">{message}</h2>
     </div>
   );
 }
