b61b589b042ecc977b4911921c0dc5ed4d2c7076
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-12 09:44:07 -0500

    asdfghh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <>
-    <div className="footer">
-    <img className="logoFoot" src="/nileLogo.png" alt="" />
-      <h4>Copyright Nile Marketplace Inc, 2023</h4>
+      <div className="footer">
+        <img className="logoFoot" src="/nileLogo.png" alt="" />
+        <h4>Copyright Nile Marketplace Inc, 2023</h4>
+        <Link to="/admin">
+          <p>Admin Access</p>
+        </Link>
       </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
