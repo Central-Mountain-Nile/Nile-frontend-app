3b266196441609c9e3d58687fb23c9ac2d901bb1
MonkComputer <bmonk1996@yahoo.com>
2023-04-12 08:54:30 -0500

    clear search history on home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useReducer, useState } from "react";
 import { useNavigate, Outlet, Link } from "react-router-dom";
 import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
-import { Categories } from "./"
+import { Categories } from "./";
 
 const Navbar = (props) => {
   const { loggedIn, setLoggedIn, setSearchTerm, setToken } = props;
@@ -15,60 +15,67 @@ const Navbar = (props) => {
 
   return (
     <>
-    <div className="fullNavBarWrapper">
-      <div id="navbar">
-        <Link to="/itemsfeed/1">
-          <img className="logoDash" src="/nileLogo.png" alt="" />
-        </Link>
-        <div className="searchBar">
-          <input
-            type="text"
-            className="search"
-            onChange={(e) => {
-              setSearchtBar(e.target.value);
+      <div className="fullNavBarWrapper">
+        <div id="navbar">
+          <Link
+            to="/itemsfeed/1"
+            onClick={() => {
+              setSearchtBar("");
+              setSearchTerm("");
             }}
-          />
-          <MagnifyingGlassCircleIcon
-            className="searchIcon"
-            onClick={() => setSearchTerm(searchBar)}
-          ></MagnifyingGlassCircleIcon>
-        </div>
+          >
+            <img className="logoDash" src="/nileLogo.png" alt="" />
+          </Link>
+          <div className="searchBar">
+            <input
+              value={searchBar}
+              type="text"
+              className="search"
+              onChange={(e) => {
+                setSearchtBar(e.target.value);
+              }}
+            />
+            <MagnifyingGlassCircleIcon
+              className="searchIcon"
+              onClick={() => setSearchTerm(searchBar)}
+            ></MagnifyingGlassCircleIcon>
+          </div>
 
-        <div className="linksBtn">
-          {loggedIn ? (
-            <div>
-              {/* <button>Hello, {props.currentUser.username}</button> */}
+          <div className="linksBtn">
+            {loggedIn ? (
+              <div>
+                {/* <button>Hello, {props.currentUser.username}</button> */}
 
-              <Link to="/">
-                <button>Home</button>
-              </Link>
-              <Link to="/cart">
-                <button>My cart</button>
-              </Link>
-              <Link to="/createitem">
-                <button>My store</button>
-              </Link>
-              <Link>
-                <button onClick={() => logout()}>Logout</button>
-              </Link>
-            </div>
-          ) : (
-            <div>
-              <Link to="/">
-                <button>Home</button>
-              </Link>
-              <Link to="/login">
-                <button>Login</button>
-              </Link>
-              <Link to="/register">
-                <button>Register</button>
-              </Link>
-            </div>
-          )}
+                <Link to="/">
+                  <button>Home</button>
+                </Link>
+                <Link to="/cart">
+                  <button>My cart</button>
+                </Link>
+                <Link to="/createitem">
+                  <button>My store</button>
+                </Link>
+                <Link>
+                  <button onClick={() => logout()}>Logout</button>
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <Link to="/">
+                  <button>Home</button>
+                </Link>
+                <Link to="/login">
+                  <button>Login</button>
+                </Link>
+                <Link to="/register">
+                  <button>Register</button>
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
         <Categories />
-        </div>
+      </div>
     </>
   );
 };
